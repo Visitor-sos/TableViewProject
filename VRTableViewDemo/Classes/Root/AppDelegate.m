014daf0d55e059bb6b3d37dbbddbac09184cf0f6
Visitor <visitorits@gmail.com>
2016-08-23 13:36:21 +0800

    添加AppDelegate类中ApplicationDidBecomeActive和ApplicationDidResignActive函数的注
    释

--- VRTableViewDemo/Classes/Root/AppDelegate.m
+++ VRTableViewDemo/Classes/Root/AppDelegate.m
@@ -17,12 +17,31 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    
+    // push project test
+    
     return YES;
 }
+
+//- (void)applicationDidBecomeActive:(UIApplication *)application {
+//    NSLog(@"application Did Become Active");
+//}
+
+//- (void)applicationWillResignActive:(UIApplication *)application {
+//    NSLog(@"application will resign active");
+//}
+
+/**
+ *  Application将要从前台进入后台时会触发此函数
+ *
+ *  @param application 当前应用程序
+ */
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    NSLog(@"application will resign active");
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -34,8 +53,15 @@
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
+/**
+ *  Application将要从后台进入前台或者挂断电话的时候会触发此函数
+ *
+ *  @param application 当前应用程序
+ */
+
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    NSLog(@"application Did Become Active");
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
